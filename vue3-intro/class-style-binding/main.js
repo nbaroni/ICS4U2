b1d3bb0ca3ec9a43d1186321a27c3bcceedeb05c
nbaroni <nbaroni@bayviewglen.ca>
2023-02-02 10:49:21 -0500

    vue expansion

--- vue3-intro/class-style-binding/main.js
+++ vue3-intro/class-style-binding/main.js
@@ -2,23 +2,11 @@ const app = Vue.createApp({
    data() {
       return {
          cart: 0,
-         product: 'Socks',
-         brand: 'Vue Mastery',
-         image: './assets/images/socks_blue.jpg',
-         inStock: false,
-         details: ['50% cotton', '30% wool', '20% polyester'],
-         variants: [
-            { id: 2234, color: 'Green', image: './assets/images/socks_green.jpg' },
-            { id: 2235, color: 'Blue', image: './assets/images/socks_blue.jpg' },
-         ]
+         premium: true,
       }
    },
    methods: {
-      addToCart() {
-         this.cart += 1
-      },
-      updateImage(variantImage) {
-         this.image = variantImage
-      }
+   },
+   computed: {
    }
 })
